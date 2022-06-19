7599c5b3ca6ab1fe8cc0c5b0991e8ec67613dc02
Tina Collier <tinacollier@Tinas-MacBook-Pro.local>
2022-06-18 20:41:19 -0400

    made pacman chomp

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
 
 
-function toggle_visibility(id) {
-    
-    var e = document.getElementById(id);
-    
-    if(e.style.visibility == 'visible')
-       e.style.visibility = 'hidden';
-    else
-       e.style.visibility = 'visible';
-    console.log('poop')
- }
 
 function blink(id) {
     var e = document.getElementById(id);
@@ -18,8 +8,32 @@ function blink(id) {
         if(e.style.visibility == 'visible')
             e.style.visibility = 'hidden';
         else
-            e.style.visibility = 'visible';
-        console.log('poop')               
-    }, 1000);
+            e.style.visibility = 'visible';             
+    }, 500);
 }
+
+const pacArray = ["./design/PacManGame.png", "./design/PacMan2.png"]
+
+let image = document.getElementById("img-to-flip")
+
+
+function chomp() {
+    let img1 = pacArray[0]
+    let img2 = pacArray[1]
+
+    
+    setInterval(() => {
+
+        console.log( image.src, img1 );
+        
+        if ( image.src.includes('PacManGame.png') ) {
+            image.src = img2;
+        } else {
+            image.src = img1;
+        }
+    }, 450);
+}
+
+
+
 
